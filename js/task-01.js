4005209d7525952199822928f3e2c7d1979f6f36
Ariel Moreno <arifonsi1@gmail.com>
2023-10-24 10:58:50 -0500

    tarea6js

---
+++ js/task-01.js
@@ -0,0 +1,5 @@
+const categoriesList = document.querySelector("ul#categories");
+
+const categoryItems =  categoriesList.querySelectorAll("li.item");
+
+console.log("numero de categorias:" + categoryItems.length);
