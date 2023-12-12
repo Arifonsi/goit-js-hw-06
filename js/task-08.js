5348128edb67eeaaef13d7f3da1833f2a63b0b60
Ariel Moreno <arifonsi1@gmail.com>
2023-12-12 16:11:29 -0500

    tare6javascript

--- js/task-08.js
+++ js/task-08.js
@@ -1,19 +1,40 @@
-function handleSubmit(event) {
-  event.preventDefault();
+// function handleSubmit(event) {
+//   event.preventDefault();
 
-  const form = event.target;
-  const email = form.elements.email.value.trim();
-  const password = form.elements.password.value.trim();
+//   const form = event.target;
+//   const email = form.elements.email.value.trim();
+//   const password = form.elements.password.value.trim();
 
-  if (email === "" || password === "") {
-    alert("Por favor, rellena todos los campos.");
-  } else {
-    const formData = {
-      email,
-      password,
-    };
+//   if (email === "" || password === "") {
+//     alert("Por favor, rellena todos los campos.");
+//   } else {
+//     const formData = {
+//       email,
+//       password,
+//     };
 
-    console.log("Datos del formulario:", formData);
-    form.reset();
-  }
-}
+//     console.log("Datos del formulario:", formData);
+//     form.reset();
+//   }
+// // }
+
+const loginForm = document.querySelector("loginForm");
+document
+  .querySelector(".login-form")
+  .addEventListener("submit", function (event) {
+    event.preventDefault();
+
+    const emailInput = this.elements.email;
+    const passwordInput = this.element.password;
+
+    if (!emailInput.value.trim() || !passwordInput.value.trim()) {
+      alert("!completar todos los campos, por faavor¡");
+    } else {
+      const formData = {
+        email: emailInput.value,
+        password: passwordInput.value,
+      };
+      console.log(formData);
+      this.reset();
+    }
+  });
